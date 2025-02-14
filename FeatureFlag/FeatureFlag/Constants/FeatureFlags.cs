80d20f13834f0ff5f2a3a606df8b1904fdf78118
Cahit Yusuf Kafadar <cahityusuf@gmail.com>
2025-02-14 16:09:48 +0300

    fix

--- FeatureFlag/FeatureFlag/Constants/FeatureFlags.cs
+++ FeatureFlag/FeatureFlag/Constants/FeatureFlags.cs
@@ -5,6 +5,4 @@ public static class FeatureFlags
     public const string FeatureA = "FeatureA";
     public const string FeatureB = "FeatureB";
     public const string FeatureC = "FeatureC";
-
-
 }
